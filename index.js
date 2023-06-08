a34bf723d4bb7ff32c65f47180d388924a71f5a6
Pelle Wessman <pelle@kodfabrik.se>
2023-06-08 13:22:00 +0200

    Export a missing type

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @typedef {import('./lib/fastify-saas-auth.js').SaasAuthIssuer} SaasAuthIssuer */
+
 export { fastifyAclPlugin } from './lib/fastify-acl.js';
 export { fastifyContextPlugin } from './lib/fastify-context.js';
 export { fastifySaasAuthPlugin } from './lib/fastify-saas-auth.js';
