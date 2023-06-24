636dd623b138ae7e57934b72ba25bfa412ed8af5
Pelle Wessman <pelle@kodfabrik.se>
2023-06-24 16:45:44 +0200

    BREAKING: More type tweaks

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,14 @@ export { fastifySaasAuthPlugin } from './lib/fastify-saas-auth.js';
 export type * from './lib/advanced-types.js';
 
 export type {
+  SaasAuthAuthenticationCallback,
+  SaasAuthCreateUserCallback,
   SaasAuthDb,
   SaasAuthIssuer,
   SaasAuthOptions,
+  SaasAuthUserinfoResponse,
 } from './lib/fastify-saas-auth.js';
+
+export type {
+  FastifyUserLoadCallback,
+} from './lib/fastify-user.js';
