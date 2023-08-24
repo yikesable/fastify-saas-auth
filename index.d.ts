6ea44b70f128e4756b99cd6ed211cf728cea7ca4
Pelle Wessman <pelle@kodfabrik.se>
2023-08-24 22:02:01 +0200

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,6 @@ export type {
   SaasAuthAuthenticationCallback,
   SaasAuthCreateUserCallback,
   SaasAuthDb,
-  SaasAuthIssuer,
   SaasAuthLoadUserCallback,
   SaasAuthOptions,
   SaasAuthPermissionCallback,
