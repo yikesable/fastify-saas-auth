a9fc3ff56d2e84679dfec4e9c4b890eb10817b4a
Pelle Wessman <pelle@kodfabrik.se>
2024-08-24 16:12:39 +0200

    Remove accidentally duplicated types

--- lib/issuer-types.d.ts
+++ lib/issuer-types.d.ts
@@ -4,29 +4,6 @@ import type {
   ValidDeclaration,
 } from '@voxpelli/type-helpers';
 
-import { MaybePromised } from '@voxpelli/type-helpers';
-import type { UserinfoResponse } from 'openid-client';
-
-export interface SaasAuthIssuerUserInfo extends UserinfoResponse {
-}
-
-export type SaasAuthIssuerPluginCallback = (
-  userinfo: SaasAuthIssuerUserInfo | undefined,
-  context: {
-    request: import('fastify').FastifyRequest;
-    name: string;
-  }
-) => MaybePromised<boolean | string | URL | void>;
-
-interface SaasAuthIssuerPluginOptions {
-  baseUrl: string
-  callback: SaasAuthIssuerPluginCallback
-  name: string,
-  prefix?: string
-  successRedirect?: string
-  failureRedirect?: string
-}
-
 export interface SaasAuthIssuer<TypeName extends AnySaasAuthIssuerType> extends ValidDeclaration<TypeName, SaasAuthIssuers> {
   // Intentionally left empty
 }
