9d5e0e9f0bb22de6d6827e33639f74110697c55b
Pelle Wessman <pelle@kodfabrik.se>
2024-10-23 13:46:26 +0200

    Improve types

--- lib/fastify-access-types.d.ts
+++ lib/fastify-access-types.d.ts
@@ -1,21 +1,36 @@
-// @ts-ignore
 import type {} from '@fastify/request-context';
 import type { FastifyRequest } from 'fastify';
+import type { PermissionContexts, PermissionContextOperations } from './rbac-types.d.ts';
 
-export type FastifyAccessPermissionCallback = (request: FastifyRequest, context: string, operation: string | undefined) => boolean;
+export type FastifyAccessPermissionCallback = <
+  Context extends PermissionContexts,
+  Operation extends PermissionContextOperations[Context]
+>(
+  request: FastifyRequest,
+  context: Context,
+  operation?: Operation | '*' | undefined
+) => boolean;
 
 export type FastifyAccessOptions = {
   permissionCallback: FastifyAccessPermissionCallback;
 };
 
+type HasPermission = <
+  Context extends PermissionContexts,
+  Operation extends PermissionContextOperations[Context]
+>(
+  context: Context,
+  operation?: Operation | '*'
+) => boolean;
+
 declare module '@fastify/request-context' {
   interface RequestContextData {
-    hasPermission?: (context: string, operation?: string | undefined) => boolean,
+    hasPermission?: HasPermission,
   }
 }
 
 declare module 'fastify' {
   interface FastifyRequest {
-    hasPermission?: (context: string, operation?: string | undefined) => boolean,
+    hasPermission?: HasPermission,
   }
 }
